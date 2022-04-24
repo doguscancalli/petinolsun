5a76dd8b2d534cd965cff37c5ed99664ee159b93
doguscancalli <dqampire@gmail.com>
2022-04-24 22:59:40 +0300

    refactor(checkbox): remove hard coded values

--- components/ui/Checkbox.js
+++ components/ui/Checkbox.js
@@ -1,18 +1,17 @@
-import { FiCheck } from 'react-icons/fi'
-
-const Checkbox = () => {
+const Checkbox = ({ label, htmlFor, ...rest }) => {
   return (
     <div className='inline-block'>
       <input
         type='checkbox'
         className='cursor-pointer opacity-0 absolute peer'
-        id='cb1'
+        id={htmlFor}
+        {...rest}
       />
       <label
         className='cursor-pointer flex items-center before:w-[30px] before:h-[30px] before:rounded-[4px] before:mr-2 before:border before:border-solid before:border-black peer-checked:before:bg-black'
-        for='cb1'
+        htmlFor={htmlFor}
       >
-        Normal
+        {label}
       </label>
     </div>
   )
