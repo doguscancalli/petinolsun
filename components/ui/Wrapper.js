af8a6c9589b20fced8299b945ecae4a70436377b
doguscancalli <dqampire@gmail.com>
2022-04-27 23:40:06 +0300

    refactor(wrapper): changed width to full

--- components/ui/Wrapper.js
+++ components/ui/Wrapper.js
@@ -1,5 +1,5 @@
 const Wrapper = ({ children }) => {
-  return <div className='max-w-screen-2xl px-5 mx-auto'>{children}</div>
+  return <div className='max-w-screen-2xl px-5 mx-auto w-full'>{children}</div>
 }
 
 export default Wrapper
