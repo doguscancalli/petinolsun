03c08784e684aa476d6607a08d8e0928461f87e7
doguscancalli <dqampire@gmail.com>
2022-04-28 16:56:45 +0300

    refactor(modal): changed height overflow

--- components/ui/Modal.js
+++ components/ui/Modal.js
@@ -4,7 +4,7 @@ const Modal = ({ children }) => {
   return (
     <div className='fixed inset-0 bg-black/80 flex items-center justify-center z-50'>
       <Wrapper>
-        <div className='bg-background rounded-2xl p-6 max-w-lg w-full mx-auto'>
+        <div className='bg-background rounded-2xl p-6 max-w-lg w-full mx-auto max-h-screen overflow-y-auto'>
           {children}
         </div>
       </Wrapper>
