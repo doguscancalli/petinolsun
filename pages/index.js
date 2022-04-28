b6654db54e32ba572828f70fa9a5e2a6e34f54a9
doguscancalli <dqampire@gmail.com>
2022-04-28 17:44:35 +0300

    refactor(pet display): change title case

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ const Home = () => {
   return (
     <>
       <Hero />
-      <PetDisplay title='Sahiplendirme İlanları' infoType='adoption' />
-      <PetDisplay title='Kayıp İlanları' infoType='lost' />
-      <PetDisplay title='Sahiplenme İlanları' infoType='ownership' />
-      <PetDisplay title='Bulunma İlanları' infoType='found' />
+      <PetDisplay title='Sahiplendirme ilanları' infoType='adoption' />
+      <PetDisplay title='Kayıp ilanları' infoType='lost' />
+      <PetDisplay title='Sahiplenme ilanları' infoType='ownership' />
+      <PetDisplay title='Bulunma ilanları' infoType='found' />
     </>
   )
 }
