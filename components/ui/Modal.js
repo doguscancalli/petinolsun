6e1b44b6f55e346aa41787b3fa27a8c6e36969fc
doguscancalli <dqampire@gmail.com>
2022-04-29 21:14:42 +0300

    refactor(modal): add y axis space

--- components/ui/Modal.js
+++ components/ui/Modal.js
@@ -4,7 +4,12 @@ const Modal = ({ children }) => {
   return (
     <div className='fixed inset-0 bg-black/80 flex items-center justify-center z-50'>
       <Wrapper>
-        <div className='bg-background rounded-2xl p-6 max-w-lg w-full mx-auto max-h-screen overflow-y-auto'>
+        <div
+          className='bg-background rounded-2xl p-6 max-w-lg w-full mx-auto overflow-y-auto'
+          style={{
+            maxHeight: 'calc(100vh - 40px)',
+          }}
+        >
           {children}
         </div>
       </Wrapper>
