6e8c67a6f73058dd2ae01dd77f273badc1ff6465
doguscancalli <dqampire@gmail.com>
2022-04-30 17:30:03 +0300

    refactor: disable send email

--- utils/index.js
+++ utils/index.js
@@ -2,7 +2,7 @@ import connectDb from './connectDb'
 import generateToken from './generateToken'
 import isAuth from './isAuth'
 import isAdmin from './isAdmin'
-import sendEmail from './sendEmail'
+// import sendEmail from './sendEmail'
 import { validateRegisterInput, validateLoginInput } from './validators'
 import advancedFiltering from './advancedFiltering'
 import apolloClient from './apolloClient'
@@ -12,7 +12,7 @@ export {
   generateToken,
   isAuth,
   isAdmin,
-  sendEmail,
+  // sendEmail,
   validateRegisterInput,
   validateLoginInput,
   advancedFiltering,
