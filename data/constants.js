d67ecf065c042fab66b7a9e9138aa7232a500dde
doguscancalli <dqampire@gmail.com>
2022-04-30 20:23:52 +0300

    feat: add constants

---
+++ data/constants.js
@@ -0,0 +1,27 @@
+export const POST_TYPE = {
+  ADOPTION: 'Sahiplendirme',
+  OWNERSHIP: 'Sahiplenme',
+  LOST: 'Kayıp',
+  FOUND: 'Bulunma',
+}
+
+export const ANIMAL = {
+  DOG: 'Köpek',
+  CAT: 'Kedi',
+  RODENT: 'Kemirgen',
+  FISH: 'Balık',
+  RABBIT: 'Tavşan',
+  OTHER: 'Diğer',
+}
+
+export const GENDER = {
+  MALE: 'Erkek',
+  FEMALE: 'Dişi',
+}
+
+export const AGE = {
+  BABY: 'Yavru',
+  YOUNG: 'Genç',
+  ADULT: 'Yetişkin',
+  OLD: 'Yaşlı',
+}
