005a26f2e547aa9e36795217bee5438f17ea71e3
doguscancalli <dqampire@gmail.com>
2022-05-01 17:53:42 +0300

    feat: add client only for apollo

--- utils/apolloClient.js
+++ utils/apolloClient.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export default new ApolloClient({
-  uri: `${process.env.APP_BASE_URL}/api/graphql`,
+  uri: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/graphql`,
   cache: new InMemoryCache(),
 })
