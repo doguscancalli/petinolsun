6bb33bd10bae1b582b37d5adf5a1214654c22dff
doguscancalli <dqampire@gmail.com>
2022-05-05 21:23:09 +0300

    refactor(select): make it dynamic

--- components/ui/Select.js
+++ components/ui/Select.js
@@ -1,16 +1,23 @@
+import { forwardRef } from 'react'
 import { FiChevronDown } from 'react-icons/fi'
 
-const Select = () => {
+const Select = ({ options, ...rest }, ref) => {
   return (
     <div className='relative'>
-      <select className='border border-black rounded-full px-6 py-3 w-full outline-none appearance-none'>
-        <option value='first'>first</option>
-        <option value='second'>second</option>
-        <option value='third'>third</option>
+      <select
+        className='border border-black rounded-full px-6 py-3 w-full outline-none appearance-none'
+        ref={ref}
+        {...rest}
+      >
+        {options.map(({ name, value }) => (
+          <option value={value} key={value}>
+            {name}
+          </option>
+        ))}
       </select>
       <FiChevronDown className='absolute top-1/2 -translate-y-1/2 right-4 pointer-events-none' />
     </div>
   )
 }
 
-export default Select
+export default forwardRef((props, ref) => Select(props, ref))
