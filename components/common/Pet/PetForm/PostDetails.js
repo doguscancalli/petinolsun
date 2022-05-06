eea5aa33300254e8226c77efee506fa64bb81c9b
doguscancalli <dqampire@gmail.com>
2022-05-07 02:42:22 +0300

    feat(post detail): add name/title input

--- components/common/Pet/PetForm/PostDetails.js
+++ components/common/Pet/PetForm/PostDetails.js
@@ -7,7 +7,7 @@ import {
 import { useForm } from 'react-hook-form'
 import { Button, Input } from '@components/ui'
 
-const PostDetails = () => {
+const PostDetails = ({ flow, step }) => {
   const dispatch = useDispatch()
 
   const {
@@ -17,6 +17,13 @@ const PostDetails = () => {
   } = useForm()
 
   const validations = {
+    name: {
+      required: `${flow[step].form.nameType} gerekli`,
+      maxLength: {
+        value: 100,
+        message: `${flow[step].form.nameType} 100 karakterden fazla olamaz`,
+      },
+    },
     description: {
       required: 'Açıklama gerekli',
       minLength: {
@@ -37,6 +44,15 @@ const PostDetails = () => {
         className='mt-4 flex flex-col gap-4'
         onSubmit={handleSubmit(onSubmit)}
       >
+        <h2 className='text-xl md:text-2xl font-bold'>
+          {flow[step].form.name}
+        </h2>
+        <Input
+          placeholder={flow[step].form.namePlaceholder}
+          error={errors?.description}
+          errorMessage={errors?.name?.message}
+          {...register('name', { ...validations.name })}
+        />
         <h2 className='text-xl md:text-2xl font-bold'>
           İlan hakkında daha fazla bilgi verin
         </h2>
