22c70253e1fbceaf1db1b24d86e04aa484976623
doguscancalli <dqampire@gmail.com>
2022-05-07 01:33:20 +0300

    feat: add auth header to apollo

--- utils/apolloClient.js
+++ utils/apolloClient.js
@@ -1,6 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { getCookie } from 'cookies-next'
+
+const httpLink = createHttpLink({
+  uri: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/graphql`,
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = getCookie('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
 
 export default new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/graphql`,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
