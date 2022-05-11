db49c00b07f8bb3b1327aca155999da406e91917
doguscancalli <dqampire@gmail.com>
2022-05-11 23:53:23 +0300

    fix(new pet post): make pet form client only

--- pages/ilan/yeni.js
+++ pages/ilan/yeni.js
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic'
 import { PetForm, StepProgress } from '@components/common'
 import { Wrapper } from '@components/ui'
 import { useSelector } from 'react-redux'
+import { ClientOnly } from '@components/shared'
 
 const Navbar = dynamic(() => import('@components/shared/Navbar'), {
   ssr: false,
@@ -23,7 +24,9 @@ const NewPetPost = () => {
         <Navbar />
         <div className='max-w-lg mx-auto mt-16'>
           <StepProgress step={handleStep()} />
-          <PetForm />
+          <ClientOnly>
+            <PetForm />
+          </ClientOnly>
         </div>
       </Wrapper>
     </>
