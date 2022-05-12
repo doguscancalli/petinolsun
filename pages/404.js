29605c7a5eded71f2456715bbfd44ad10a0163f6
doguscancalli <dqampire@gmail.com>
2022-05-12 03:56:37 +0300

    feat: add 404 page

---
+++ pages/404.js
@@ -0,0 +1,28 @@
+import { Button } from '@components/ui'
+import Image from 'next/image'
+
+const NotFound = () => {
+  return (
+    <div className='flex items-center justify-center flex-col h-screen'>
+      <div
+        className='relative max-w-xs sm:max-w-sm w-full'
+        style={{ aspectRatio: '1/1' }}
+      >
+        <Image
+          src='/images/not-found-illustration.svg'
+          alt='Sayfa Bulunamadı'
+          // width='100%'
+          // height='100%'
+          layout='fill'
+          objectFit='contain'
+        />
+      </div>
+      <div className='flex flex-col gap-2'>
+        <h2 className='text-2xl md:text-3xl font-bold'>Sayfa bulunamadı</h2>
+        <Button href='/'>Anasayfaya Dön</Button>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
