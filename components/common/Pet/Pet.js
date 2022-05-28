4fc0a28193af6280ab1e24e4c294acc3bcee9b4b
doguscancalli <dqampire@gmail.com>
2022-05-28 21:46:10 +0300

    refactor(pet): location text

--- components/common/Pet/Pet.js
+++ components/common/Pet/Pet.js
@@ -52,7 +52,7 @@ const Pet = ({ info, infoType, post, totalDocs }) => {
           <div className='w-full p-4 pt-6 bg-gradient-to-t from-black/60 to-black/0 text-white mt-auto z-0'>
             <h2 className='text-base md:text-xl font-bold'>{name}</h2>
             <div className='flex justify-between mt-2'>
-              <p className='text-black-200 break-all flex-1'>{`${city},${district}`}</p>
+              <p className='text-black-200 break-all flex-1'>{`${city}, ${district}`}</p>
               <p className='text-black-200 place-self-end text-right flex-1'>
                 <Moment locale='tr' fromNow>
                   {createdAt}
