0d1e10a74aadcc8c4c5269fb6fd44043151c1980
doguscancalli <dqampire@gmail.com>
2022-05-30 02:37:59 +0300

    feat: make text slide if larger than parent

--- components/common/Pet/Pet.js
+++ components/common/Pet/Pet.js
@@ -1,10 +1,14 @@
-import { useRouter } from 'next/router'
+import { useRef } from 'react'
+import { useRouter, withRouter } from 'next/router'
 import Image from 'next/image'
 import Moment from 'react-moment'
 import 'moment/locale/tr'
 
 const Pet = ({ info, infoType, post, totalDocs }) => {
   const { slug, name, city, district, photos, createdAt } = post || {}
+
+  const nameParentRef = useRef(null)
+  const nameRef = useRef(null)
 
   const router = useRouter()
 
@@ -34,6 +38,21 @@ const Pet = ({ info, infoType, post, totalDocs }) => {
     return types[type]
   }
 
+  const onMouseEnter = () => {
+    const nameParentWidth = nameParentRef.current.clientWidth
+    const nameWidth = nameRef.current.clientWidth
+
+    if (nameWidth > nameParentWidth) {
+      nameRef.current.style.transform = `translateX(-${
+        nameWidth - nameParentWidth
+      }px)`
+    }
+  }
+
+  const onMouseLeave = () => {
+    nameRef.current.style.transform = 'translateX(0%)'
+  }
+
   {
     if (!info) {
       return (
@@ -41,6 +60,8 @@ const Pet = ({ info, infoType, post, totalDocs }) => {
           className={`rounded-2xl overflow-hidden cursor-pointer flex items-start relative`}
           style={{ aspectRatio: '1 / 1.5' }}
           onClick={() => router.push(`/ilan/${slug}`)}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
         >
           <Image
             src={photos[0]}
@@ -50,7 +71,17 @@ const Pet = ({ info, infoType, post, totalDocs }) => {
             className='-z-10'
           />
           <div className='w-full p-4 pt-6 bg-gradient-to-t from-black/60 to-black/0 text-white mt-auto z-0'>
-            <h2 className='text-base md:text-xl font-bold'>{name}</h2>
+            <div
+              className='overflow-hidden relative h-6 md:h-7'
+              ref={nameParentRef}
+            >
+              <h2
+                className='text-base md:text-xl font-bold whitespace-nowrap absolute left-0 top-0 transition duration-[2s] ease-linear'
+                ref={nameRef}
+              >
+                {name}
+              </h2>
+            </div>
             <div className='flex justify-between mt-2'>
               <p className='text-black-200 break-all flex-1'>{`${city}, ${district}`}</p>
               <p className='text-black-200 place-self-end text-right flex-1'>
