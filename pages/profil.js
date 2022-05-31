3f61e14616fe2451867ad6eea556fa66bb4adbbc
doguscancalli <dqampire@gmail.com>
2022-05-31 21:31:21 +0300

    refactor(profile): remove react import

--- pages/profil.js
+++ pages/profil.js
@@ -2,7 +2,6 @@ import { ProfileView } from '@components/common'
 import { ClientOnly } from '@components/shared'
 import { Wrapper } from '@components/ui'
 import dynamic from 'next/dynamic'
-import React from 'react'
 
 const Navbar = dynamic(() => import('@components/shared/Navbar'), {
   ssr: false,
