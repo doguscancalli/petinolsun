6bba0a0a0eb1a1c008f89e59c11873a57a08df47
doguscancalli <dqampire@gmail.com>
2022-05-31 23:51:14 +0300

    feat(isAuth): add cookie check

--- utils/isAuth.js
+++ utils/isAuth.js
@@ -2,6 +2,8 @@ import { verify } from 'jsonwebtoken'
 
 export default (context) => {
   const authHeader = context.req.headers.authorization
+  const authCookie = context.req.cookies.token
+
   if (authHeader) {
     const token = authHeader.split('Bearer ')[1]
     if (token) {
@@ -14,6 +16,18 @@ export default (context) => {
       }
     }
     throw new Error("Authentication token must be 'Bearer [token]")
+  }
+  if (authCookie) {
+    const token = authCookie
+    if (token) {
+      try {
+        const user = verify(token, process.env.JWT_SECRET)
+        context.req.user = user
+        return user
+      } catch (err) {
+        throw new Error('Invalid/Expired token')
+      }
+    }
   }
   throw new Error('Authorization header must be provided')
 }
