43824bed6f3ba8af60cf8e2f63d8ebb62a95ad93
doguscancalli <dqampire@gmail.com>
2022-06-02 13:54:04 +0300

    refactor(Wrapper): add className prop

--- components/ui/Wrapper.js
+++ components/ui/Wrapper.js
@@ -1,5 +1,9 @@
-const Wrapper = ({ children }) => {
-  return <div className='max-w-screen-2xl px-5 mx-auto w-full'>{children}</div>
+const Wrapper = ({ className, children }) => {
+  return (
+    <div className={`max-w-screen-2xl px-5 mx-auto w-full ${className}`}>
+      {children}
+    </div>
+  )
 }
 
 export default Wrapper
