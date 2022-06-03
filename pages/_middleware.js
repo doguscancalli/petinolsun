84a60727e3620315f6d911e5a75f93a53d8165d2
doguscancalli <dqampire@gmail.com>
2022-06-03 23:34:37 +0300

    feat(middleware): add admin routes

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,7 +6,13 @@ export default async function middleware(req) {
   const { token } = cookies
   const domain = req.nextUrl.clone()
 
-  const authRoutes = ['/ilan/yeni', '/profil']
+  const authRoutes = ['/ilan/yeni', '/profil', '/ayarlar']
+  const adminRoutes = [
+    '/admin',
+    '/admin/ilan',
+    '/admin/gonderi',
+    '/admin/kullanici',
+  ]
 
   if (authRoutes.some((r) => url.includes(r))) {
     if (!token) {
@@ -15,6 +21,23 @@ export default async function middleware(req) {
     }
     const isValid = await jwt.verify(token, process.env.JWT_SECRET)
     if (!isValid) {
+      domain.pathname = '/giris'
+      return NextResponse.redirect(domain)
+    }
+  }
+
+  if (adminRoutes.some((r) => url.includes(r))) {
+    if (!token) {
+      domain.pathname = '/giris'
+      return NextResponse.redirect(domain)
+    }
+    const isValid = await jwt.verify(token, process.env.JWT_SECRET)
+    if (!isValid) {
+      domain.pathname = '/giris'
+      return NextResponse.redirect(domain)
+    }
+    const { isAdmin } = jwt.decode(token)
+    if (!isAdmin) {
       domain.pathname = '/giris'
       return NextResponse.redirect(domain)
     }
