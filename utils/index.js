3ffe9d4ca5dc3c92a039a24f85a65071708e10b9
doguscancalli <dqampire@gmail.com>
2022-06-03 13:20:43 +0300

    refactor: remove sendMail util

--- utils/index.js
+++ utils/index.js
@@ -2,7 +2,6 @@ import connectDb from './connectDb'
 import generateToken from './generateToken'
 import isAuth from './isAuth'
 import isAdmin from './isAdmin'
-// import sendEmail from './sendEmail'
 import { validateRegisterInput, validateLoginInput } from './validators'
 import advancedFiltering from './advancedFiltering'
 import apolloClient from './apolloClient'
@@ -16,7 +15,6 @@ export {
   generateToken,
   isAuth,
   isAdmin,
-  // sendEmail,
   validateRegisterInput,
   validateLoginInput,
   advancedFiltering,
