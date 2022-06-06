f8cf0cc1f4f54a167163657d4fb3531b1694d753
doguscancalli <dqampire@gmail.com>
2022-06-07 01:46:12 +0300

    feat: add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -8,17 +8,21 @@ import 'react-toastify/dist/ReactToastify.css'
 import 'moment/locale/tr'
 import { DefaultSeo } from 'next-seo'
 import SEO from '../next-seo.config'
+import { GoogleAnalytics, usePagesViews } from 'nextjs-google-analytics'
 
 const Noop = ({ children }) => <>{children}</>
 
 function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout ?? Noop
 
+  usePagesViews()
+
   return (
     <Provider store={store}>
       <ApolloProvider client={apolloClient}>
         <ToastContainer position='top-center' />
         <Layout>
+          <GoogleAnalytics />
           <DefaultSeo {...SEO} />
           <Component {...pageProps} />
         </Layout>
