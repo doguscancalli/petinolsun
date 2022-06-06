9561db39f6d92f01f8f4b664a9b1109f6bd55644
doguscancalli <dqampire@gmail.com>
2022-06-07 01:19:27 +0300

    fix(Profile): import

--- pages/profil.js
+++ pages/profil.js
@@ -1,6 +1,7 @@
 import { DefaultLayout, ProfileView } from '@components/common'
 import { ClientOnly } from '@components/shared'
 import { Wrapper } from '@components/ui'
+import { NextSeo } from 'next-seo'
 
 const Profile = () => {
   return (
