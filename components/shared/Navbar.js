ec61b4207d1908db76a28f108268eb8bef6b060d
doguscancalli <dqampire@gmail.com>
2022-06-07 16:22:04 +0300

    refactor: navbar responsivity

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -64,12 +64,12 @@ const Navbar = ({ className }) => {
   return (
     <div className='relative w-full'>
       <nav
-        className={`bg-white rounded-full flex grow items-center py-2 pl-4 pr-2 sm:py-3 sm:pl-6 sm:pr-3 mt-5 md:mt-8 justify-between ${className}`}
+        className={`bg-white rounded-full flex grow items-center py-2 pl-4 pr-2 md:py-3 md:pl-6 md:pr-3 mt-5 md:mt-8 justify-between ${className}`}
       >
         <Link href='/'>
           <a className='font-bold'>petinolsun</a>
         </Link>
-        <ul className='hidden sm:flex items-center ml-auto'>
+        <ul className='hidden md:flex items-center ml-auto'>
           {navLinks.map((link, index) => {
             if (link.visibility === 'guest' && auth.user) return null
             if (link.visibility === 'user' && !auth.user) return null
@@ -78,14 +78,14 @@ const Navbar = ({ className }) => {
         </ul>
         <div className='hidden md:block'>{auth?.user && <UserDisplay />}</div>
         <button
-          className='sm:hidden rounded-full bg-black p-[10px]'
+          className='md:hidden rounded-full bg-black p-[10px]'
           onClick={() => setToggleMenu(!toggleMenu)}
         >
           <FiMenu className='text-white' />
         </button>
       </nav>
       {toggleMenu && (
-        <ul className='block sm:hidden bg-black text-white mt-2 rounded-2xl text-center p-6 absolute right-0 left-0 z-50'>
+        <ul className='flex flex-col items-center md:hidden bg-black text-white mt-2 rounded-2xl text-center p-6 absolute right-0 left-0 z-50'>
           {navLinks.map((link, index) => {
             if (link.visibility === 'guest' && auth.user) return null
             return (
