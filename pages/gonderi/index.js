20a2b423c3c2dfbd8f84adb8cdce024a5636dc97
doguscancalli <dqampire@gmail.com>
2022-06-08 13:49:38 +0300

    feat(NewPost): auth check

--- pages/gonderi/index.js
+++ pages/gonderi/index.js
@@ -3,9 +3,20 @@ import { useState } from 'react'
 import { DefaultLayout, NewPost, PostDisplay } from '@components/common'
 import { ClientOnly } from '@components/shared'
 import { NextSeo } from 'next-seo'
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
 
 const PostPage = () => {
   const [toggleModal, setToggleModal] = useState(false)
+
+  const { user } = useSelector((state) => state.auth)
+
+  const router = useRouter()
+
+  const handleNewPost = () => {
+    if (!user) return router.push('/giris')
+    setToggleModal(true)
+  }
 
   return (
     <Wrapper>
@@ -18,7 +29,7 @@ const PostPage = () => {
         )}
         <div className='flex justify-between items-center mb-8'>
           <h1 className='text-2xl md:text-4xl font-bold'>Son gönderiler</h1>
-          <Button onClick={() => setToggleModal(true)}>Yeni Gönderi</Button>
+          <Button onClick={handleNewPost}>Yeni Gönderi</Button>
         </div>
         <ClientOnly>
           <PostDisplay className='mt-6' />
